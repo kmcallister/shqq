a1143e6890b514f80618e492f051ac601bcfe040
Keegan McAllister <mcallister.keegan@gmail.com>
2011-09-25 06:12:03 -0400

    capture exit code

--- System/ShQQ.hs
+++ System/ShQQ.hs
@@ -4,20 +4,23 @@
 
 module System.ShQQ
     ( sh
-    , readShellCommand
+    , shc
+    , readShell
+    , readShellWithCode
     , showNonString
     ) where
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
 import Control.Applicative
-import Control.Exception ( evaluate )
+import Control.Exception ( evaluate, throwIO )
 import Data.Char
 import Data.Foldable ( asum )
 import Data.Typeable ( Typeable, cast )
 import Text.Parsec hiding ( (<|>), many )
 import Text.Parsec.String
 import System.IO
+import System.Exit
 
 import qualified System.Posix.Escape as E
 import qualified System.Process      as P
@@ -49,35 +52,46 @@ parseToks = many part where
             ( var <|> between (char '{') (char '}') var )
       , Lit <$> some (noneOf "$\\") ]
 
-readShellCommand :: String -> IO String
-readShellCommand cmd = do
+readShellWithCode :: String -> IO (ExitCode, String)
+readShellWithCode cmd = do
     (Nothing, Just hOut, Nothing, hProc) <- P.createProcess $
         (P.shell cmd) { P.std_out = P.CreatePipe }
     out <- hGetContents hOut
     _   <- evaluate (length out)
     hClose hOut
-    _   <- P.waitForProcess hProc
-    return out
+    ec  <- P.waitForProcess hProc
+    return (ec, out)
 
-mkExp :: [Tok] -> Q Exp
-mkExp toks = [| readShellCommand (concat $strs) |] where
+readShell :: String -> IO String
+readShell cmd = do
+    (ec, out) <- readShellWithCode cmd
+    case ec of
+        ExitSuccess -> return out
+        _ -> throwIO ec
+
+mkExp :: Q Exp -> [Tok] -> Q Exp
+mkExp reader toks = [| $reader (concat $strs) |] where
     strs = listE (map f toks)
     var  = varE . mkName
     f (Lit     x) = [| x |]
     f (VarOne  v) = [| E.escape (showNonString $(var v)) |]
     f (VarMany v) = [| showNonString $(var v) |]
 
-shExp :: String -> Q Exp
-shExp xs = case parse parseToks "System.ShQQ expression" xs of
+shExp :: Q Exp -> String -> Q Exp
+shExp reader xs = case parse parseToks "System.ShQQ expression" xs of
     Left  e -> error ('\n' : show e)
-    Right t -> mkExp t
+    Right t -> mkExp reader t
 
-sh :: QuasiQuoter
-sh = QuasiQuoter
-    { quoteExp  = shExp
+baseQQ :: QuasiQuoter
+baseQQ = QuasiQuoter
+    { quoteExp  = error "internal error in System.ShQQ"
     , quotePat  = const (error "no pattern quote for System.ShQQ")
 #if MIN_VERSION_template_haskell(2,5,0)
     , quoteType = const (error "no type quote for System.ShQQ")
     , quoteDec  = const (error "no decl quote for System.ShQQ")
 #endif
     }
+
+sh, shc :: QuasiQuoter
+sh  = baseQQ { quoteExp = shExp [| readShell |] }
+shc = baseQQ { quoteExp = shExp [| readShellWithCode |] }
